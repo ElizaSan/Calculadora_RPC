62681426cf7d13aafacbc6b68d059db897679c2a
Miri <paletaleblue@outlook.com>
2025-10-03 10:10:23 -0600

    Se arregla una funcion block para que solo reciba una solictud a la vez, se agrega la informacion de la IP proveniente

--- servidor.py
+++ servidor.py
@@ -2,14 +2,25 @@ import grpc
 from concurrent import futures
 import calculadora_pb2
 import calculadora_pb2_grpc
-import time
+import time, threading
 
 class CalculadoraServicer(calculadora_pb2_grpc.CalculadoraServicer):
+    lock = threading.Lock()
 
+    def block(self, request, context):
+        if not self.lock.acquire(blocking=False):
+            context.set_code(grpc.StatusCode.RESOURCE_EXHAUSTED)
+            context.set_details("Servidor ocupado. Intenta más tarde.")
+            return calculadora_pb2.Resultado()
     
     def Suma(self, request, context):
+
+        self.block(request, context)
+
         try:
-            print(f"Solicitud de suma recibida: {request.a} + {request.b}")
+            peer = context.peer()
+            print(f"Conexión recibida de: {peer}")
+            print(f"Solicitud de suma: {request.a} + {request.b}")
             resultado = request.a + request.b
             time.sleep(5)  
             return calculadora_pb2.Resultado(valor=resultado)
@@ -18,26 +29,37 @@ class CalculadoraServicer(calculadora_pb2_grpc.CalculadoraServicer):
             context.set_details("Error interno al sumar.")
             context.set_code(grpc.StatusCode.INTERNAL)
             return calculadora_pb2.Resultado()
-
+        finally: 
+            self.lock.release()
 
     
     def Resta(self, request, context):
+
+        self.block(request, context)
         try:
+            peer = context.peer()
+            print(f"Conexión recibida de: {peer}")
             print(f"Solicitud de resta recibida: {request.a} + {request.b}")
-            resultado = request.a - request.b
             time.sleep(5) 
+            resultado = request.a - request.b
             return calculadora_pb2.Resultado(valor=resultado)
         except Exception as e:
             print(f"Error procesando la solicitud: {e}")
             context.set_details("Error interno al restar.")
             context.set_code(grpc.StatusCode.INTERNAL)
             return calculadora_pb2.Resultado()
+        finally: 
+            self.lock.release()
 
 
 
     def Multiplicacion(self, request, context):
+        self.block(request, context)
         try:
+            peer = context.peer()
+            print(f"Conexión recibida de: {peer}")
             print(f"Solicitud de multiplicacion recibida: {request.a} + {request.b}")
+            time.sleep(5) 
             resultado = request.a * request.b
             return calculadora_pb2.Resultado(valor=resultado)
         except Exception as e:
@@ -45,22 +67,29 @@ class CalculadoraServicer(calculadora_pb2_grpc.CalculadoraServicer):
             context.set_details("Error interno al multiplicar.")
             context.set_code(grpc.StatusCode.INTERNAL)
             return calculadora_pb2.Resultado()
+        finally: 
+            self.lock.release()
     
     def Division(self, request, context):
-        print(f"Solicitud de suma recibida: {request.a} + {request.b}")
+        self.block(request, context)
+        print(f"Solicitud de division recibida: {request.a} + {request.b}")
         try:
+            peer = context.peer()
+            print(f"Conexión recibida de: {peer}")
             if request.b == 0:
                 context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
                 context.set_details("No se puede dividir entre cero")
                 return calculadora_pb2.Resultado(valor=0)
-            resultado = request.a / request.b
             time.sleep(5) 
+            resultado = request.a / request.b
             return calculadora_pb2.Resultado(valor=resultado)  
         except Exception as e:
             print(f"Error procesando la solicitud: {e}")
             context.set_details("Error interno al dividir.")
             context.set_code(grpc.StatusCode.INTERNAL)
             return calculadora_pb2.Resultado()
+        finally: 
+            self.lock.release()
 
 
 def servir():
